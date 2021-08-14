94c33fabcbfe8a7f037eb52492823bda4ef8d168
Gabriel de Matos Hainzenreder <44183128+GabrielDimaa@users.noreply.github.com>
2021-08-13 22:52:38 -0300

    Adicionado chaves do Parse e Mux no ponto .env

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,24 @@
 import 'package:flutter/material.dart';
-import 'app/app_widget.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:parse_server_sdk_flutter/parse_server_sdk.dart';
 
-void main() => runApp(AppWidget());
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await dotenv.load(fileName: ".env");
+
+  final keyApplicationId = dotenv.env['PARSE_SERVER_APP_ID'];
+  final keyClientKey = dotenv.env['PARSE_SERVER_CLIENT_KEY'];
+  final keyParseServerUrl = 'https://parseapi.back4app.com';
+
+  await Parse().initialize(keyApplicationId!, keyParseServerUrl,
+      clientKey: keyClientKey, autoSendSessionId: true);
+
+  var firstObject = ParseObject('FirstClass')
+    ..set(
+        'message', 'Hey ! First message from Flutter. Parse is now connected');
+  await firstObject.save();
+
+  print('done');
+}
