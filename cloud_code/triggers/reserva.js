825e23b579a3e5f3a4513d9c30f77bc51786782e
Gabriel de Matos Hainzenreder <44183128+GabrielDimaa@users.noreply.github.com>
2021-10-21 20:11:48 -0300

    Correção na trigger de reservas

--- cloud_code/triggers/reserva.js
+++ cloud_code/triggers/reserva.js
@@ -1,21 +1,23 @@
 Parse.Cloud.beforeSave("Reserva", async (request) => {
-    const compraProdutoId = request.object.get("idProduto");
-    const compraProdutoQtd = request.object.get("quantidade");
-    
-    const query = new Parse.Query("Produto");
-    query.equalTo("objectId", compraProdutoId);
-    
-    const result = await query.first();
+    if (request.object.isNew()) {
+        const compraProdutoId = request.object.get("idProduto");
+        const compraProdutoQtd = request.object.get("quantidade");
+        
+        const query = new Parse.Query("Produto");
+        query.equalTo("objectId", compraProdutoId);
+        
+        const result = await query.first();
 
-    const qtdEmEstoque = result.get("quantidade");
-    
-    if (qtdEmEstoque <= 0) {
-        throw "Sem unidades disponíveis."
-    } else if (qtdEmEstoque < compraProdutoQtd) {
-        throw "Quantidade superior ao que está disponível para compra.";
-    } else {
-        result.set("quantidade", qtdEmEstoque - compraProdutoQtd);
+        const qtdEmEstoque = result.get("quantidade");
+        
+        if (qtdEmEstoque <= 0) {
+            throw "Sem unidades disponíveis."
+        } else if (qtdEmEstoque < compraProdutoQtd) {
+            throw "Quantidade superior ao que está disponível para compra.";
+        } else {
+            result.set("quantidade", qtdEmEstoque - compraProdutoQtd);
 
-        await result.save();
+            await result.save();
+        }
     }
 });
